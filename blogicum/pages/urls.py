f6492e911e7a37095aae5e7ee309cf23aa4ac7b3
foxyfeed <smarkov906@gmail.com>
2026-05-22 15:32:23 +0300

    upload

--- blogicum/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "pages"
-urlpatterns = [
-    path("about/", views.AboutListView.as_view(), name="about"),
-    path("rules/", views.RulesListView.as_view(), name="rules"),
-]
